7e9c6276ff3bd9c8f8ca54458f186481af313fe1
cjtabuel <31078420+cjtabuel@users.noreply.github.com>
2020-10-09 15:28:40 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,57 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  entry: {
-    index: ['babel-polyfill', './src/app.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'public/scripts'),
-    filename: '[name]-bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react'
-          ]
+module.exports = () => {
+  return {
+    entry: {
+      index: ['babel-polyfill', './src/app.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'public/'),
+      filename: '[name]-bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
         }
-      }
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader', // inject styles into DOM
-        'css-loader', // compiles css into js
-        'sass-loader' // compiles sass into css
-      ]
-    }]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    historyApiFallback: true,
-    publicPath: '/scripts/'
-  },
-  devtool: 'source-map'
+      }, {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      })
+    ],
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/scripts/'
+    },
+    devtool: 'source-map'
+  }
 }
